e9cb55d62d3145babe3812d192c940f8123b5e5e
iver gentz <ivergentz@gmail.com>
2021-11-16 13:01:40 +0100

    Prepared header component, added h2 and MobileIcon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,53 @@
-import React from 'react'
+import { React, useState } from 'react'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import styled from 'styled-components'
 
 const Header = () => {
-  return <div></div>
+  const [toggle, setToggle] = useState(false)
+
+  const handleClick = () => {
+    setToggle(!toggle)
+  }
+
+  return (
+    <>
+      <HeaderContainer>
+        <h2>i.gentz</h2>
+        <IconContainer>
+          <MobileIcon onClick={handleClick} />
+          {console.log(toggle)}
+        </IconContainer>
+      </HeaderContainer>
+    </>
+  )
 }
 
 export default Header
+
+const HeaderContainer = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  top: 0;
+  position: fixed;
+  width: 100%;
+  height: 5rem;
+  background: var(--bg-white);
+  color: var(--text-dark);
+  z-index: 100;
+
+  h2 {
+    margin-left: 2rem;
+    cursor: pointer;
+  }
+`
+
+const IconContainer = styled.div`
+  margin-right: 2rem;
+`
+
+const MobileIcon = styled(GiHamburgerMenu)`
+  height: 1.3rem;
+  width: 1.3rem;
+  cursor: pointer;
+`
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,114 @@
 import React from 'react'
+import Header from '../components/Header'
 
 const Homepage = () => {
-  return <div>Moin</div>
+  return (
+    <>
+      <Header />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
+        fugit officiis necessitatibus hic? Inventore, accusamus quasi reiciendis
+        harum, laudantium repellendus facilis expedita aliquid nam maxime odit
+        hic eum optio? Asperiores! Lorem ipsum dolor, sit amet consectetur
+        adipisicing elit. Tempora non repellat earum culpa quia expedita dolorem
+        placeat quae, rerum dignissimos harum rem temporibus dolore.
+        Perspiciatis accusamus neque saepe nisi est? Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Animi tempora architecto, eligendi quae
+        aliquid quas minus eos minima excepturi eum ab? Necessitatibus sapiente
+        quam eveniet omnis perferendis officiis molestiae quibusdam!Lorem ipsum
+        dolor sit amet, consectetur adipisicing elit. Reiciendis fugit officiis
+        necessitatibus hic? Inventore, accusamus quasi reiciendis harum,
+        laudantium repellendus facilis expedita aliquid nam maxime odit hic eum
+        optio? Asperiores! Lorem ipsum dolor, sit amet consectetur adipisicing
+        elit. Tempora non repellat earum culpa quia expedita dolorem placeat
+        quae, rerum dignissimos harum rem temporibus dolore. Perspiciatis
+        accusamus neque saepe nisi est? Lorem, ipsum dolor sit amet consectetur
+        adipisicing elit. Animi tempora architecto, eligendi quae aliquid quas
+        minus eos minima excepturi eum ab? Necessitatibus sapiente quam eveniet
+        omnis perferendis officiis molestiae quibusdam!Lorem ipsum dolor sit
+        amet, consectetur adipisicing elit. Reiciendis fugit officiis
+        necessitatibus hic? Inventore, accusamus quasi reiciendis harum,
+        laudantium repellendus facilis expedita aliquid nam maxime odit hic eum
+        optio? Asperiores! Lorem ipsum dolor, sit amet consectetur adipisicing
+        elit. Tempora non repellat earum culpa quia expedita dolorem placeat
+        quae, rerum dignissimos harum rem temporibus dolore. Perspiciatis
+        accusamus neque saepe nisi est? Lorem, ipsum dolor sit amet consectetur
+        adipisicing elit. Animi tempora architecto, eligendi quae aliquid quas
+        minus eos minima excepturi eum ab? Necessitatibus sapiente quam eveniet
+        omnis perferendis officiis molestiae quibusdam!Lorem ipsum dolor sit
+        amet, consectetur adipisicing elit. Reiciendis fugit officiis
+        necessitatibus hic? Inventore, accusamus quasi reiciendis harum,
+        laudantium repellendus facilis expedita aliquid nam maxime odit hic eum
+        optio? Asperiores! Lorem ipsum dolor, sit amet consectetur adipisicing
+        elit. Tempora non repellat earum culpa quia expedita dolorem placeat
+        quae, rerum dignissimos harum rem temporibus dolore. Perspiciatis
+        accusamus neque saepe nisi est? Lorem, ipsum dolor sit amet consectetur
+        adipisicing elit. Animi tempora architecto, eligendi quae aliquid quas
+        minus eos minima excepturi eum ab? Necessitatibus sapiente quam eveniet
+        omnis perferendis officiis molestiae quibusdam!Lorem ipsum dolor sit
+        amet, consectetur adipisicing elit. Reiciendis fugit officiis
+        necessitatibus hic? Inventore, accusamus quasi reiciendis harum,
+        laudantium repellendus facilis expedita aliquid nam maxime odit hic eum
+        optio? Asperiores! Lorem ipsum dolor, sit amet consectetur adipisicing
+        elit. Tempora non repellat earum culpa quia expedita dolorem placeat
+        quae, rerum dignissimos harum rem temporibus dolore. Perspiciatis
+        accusamus neque saepe nisi est? Lorem, ipsum dolor sit amet consectetur
+        adipisicing elit. Animi tempora architecto, eligendi quae aliquid quas
+        minus eos minima excepturi eum ab? Necessitatibus sapiente quam eveniet
+        omnis perferendis officiis molestiae quibusdam!Lorem ipsum dolor sit
+        amet, consectetur adipisicing elit. Reiciendis fugit officiis
+        necessitatibus hic? Inventore, accusamus quasi reiciendis harum,
+        laudantium repellendus facilis expedita aliquid nam maxime odit hic eum
+        optio? Asperiores! Lorem ipsum dolor, sit amet consectetur adipisicing
+        elit. Tempora non repellat earum culpa quia expedita dolorem placeat
+        quae, rerum dignissimos harum rem temporibus dolore. Perspiciatis
+        accusamus neque saepe nisi est? Lorem, ipsum dolor sit amet consectetur
+        adipisicing elit. Animi tempora architecto, eligendi quae aliquid quas
+        minus eos minima excepturi eum ab? Necessitatibus sapiente quam eveniet
+        omnis perferendis officiis molestiae quibusdam!Lorem ipsum dolor sit
+        amet, consectetur adipisicing elit. Reiciendis fugit officiis
+        necessitatibus hic? Inventore, accusamus quasi reiciendis harum,
+        laudantium repellendus facilis expedita aliquid nam maxime odit hic eum
+        optio? Asperiores! Lorem ipsum dolor, sit amet consectetur adipisicing
+        elit. Tempora non repellat earum culpa quia expedita dolorem placeat
+        quae, rerum dignissimos harum rem temporibus dolore. Perspiciatis
+        accusamus neque saepe nisi est? Lorem, ipsum dolor sit amet consectetur
+        adipisicing elit. Animi tempora architecto, eligendi quae aliquid quas
+        minus eos minima excepturi eum ab? Necessitatibus sapiente quam eveniet
+        omnis perferendis officiis molestiae quibusdam!Lorem ipsum dolor sit
+        amet, consectetur adipisicing elit. Reiciendis fugit officiis
+        necessitatibus hic? Inventore, accusamus quasi reiciendis harum,
+        laudantium repellendus facilis expedita aliquid nam maxime odit hic eum
+        optio? Asperiores! Lorem ipsum dolor, sit amet consectetur adipisicing
+        elit. Tempora non repellat earum culpa quia expedita dolorem placeat
+        quae, rerum dignissimos harum rem temporibus dolore. Perspiciatis
+        accusamus neque saepe nisi est? Lorem, ipsum dolor sit amet consectetur
+        adipisicing elit. Animi tempora architecto, eligendi quae aliquid quas
+        minus eos minima excepturi eum ab? Necessitatibus sapiente quam eveniet
+        omnis perferendis officiis molestiae quibusdam!Lorem ipsum dolor sit
+        amet, consectetur adipisicing elit. Reiciendis fugit officiis
+        necessitatibus hic? Inventore, accusamus quasi reiciendis harum,
+        laudantium repellendus facilis expedita aliquid nam maxime odit hic eum
+        optio? Asperiores! Lorem ipsum dolor, sit amet consectetur adipisicing
+        elit. Tempora non repellat earum culpa quia expedita dolorem placeat
+        quae, rerum dignissimos harum rem temporibus dolore. Perspiciatis
+        accusamus neque saepe nisi est? Lorem, ipsum dolor sit amet consectetur
+        adipisicing elit. Animi tempora architecto, eligendi quae aliquid quas
+        minus eos minima excepturi eum ab? Necessitatibus sapiente quam eveniet
+        omnis perferendis officiis molestiae quibusdam!Lorem ipsum dolor sit
+        amet, consectetur adipisicing elit. Reiciendis fugit officiis
+        necessitatibus hic? Inventore, accusamus quasi reiciendis harum,
+        laudantium repellendus facilis expedita aliquid nam maxime odit hic eum
+        optio? Asperiores! Lorem ipsum dolor, sit amet consectetur adipisicing
+        elit. Tempora non repellat earum culpa quia expedita dolorem placeat
+        quae, rerum dignissimos harum rem temporibus dolore. Perspiciatis
+        accusamus neque saepe nisi est? Lorem, ipsum dolor sit amet consectetur
+        adipisicing elit. Animi tempora architecto, eligendi quae aliquid quas
+        minus eos minima excepturi eum ab? Necessitatibus sapiente quam eveniet
+        omnis perferendis officiis molestiae quibusdam!
+      </p>
+    </>
+  )
 }
 
 export default Homepage
